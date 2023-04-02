f76c268898a291e905d1ccf8da2fc26735862f16
aminerve <122647915+aminerve@users.noreply.github.com>
2023-04-02 12:03:48 -0400

    template complete

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,7 +1,67 @@
 import React from 'react'
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component'
+import 'react-vertical-timeline-component/style.min.css'
+import { School, Work } from '@material-ui/icons'
+import '../styles/Experience.css'
 
 export default function Experience() {
   return (
-    <div>Experience</div>
+    <div className='experience'>
+      <VerticalTimeline lineColor='#3e497a'>
+        <VerticalTimelineElement className='vertical-timeline-element--education' date='2018 - 2022' iconStyle={{background: "#3e497a", color: '#fff'}}
+        icon={<School/>}>
+          <h3 className='vertical-timeline-element-title'> My Random High School, Random Place, Random State</h3>
+          <p> High School Diploma</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2023 - 2023"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<School />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Per Scholas 15 Week Software Engineering Bootcamp
+          </h3>
+
+          <h4 className="vertical-timeline-element-subtitle">
+            Bachelor's Degree
+          </h4>
+
+          <p> Computer Science</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2018 - 2020"
+          iconStyle={{ background: "#e9d35b", color: "#fff" }}
+          icon={<Work />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Back End Engineer - Google
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            San Francisco, CA
+          </h4>
+          <p>Developed the backend infrastructure for 3 projects.</p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2020 - present"
+          iconStyle={{ background: "#e9d35b", color: "#fff" }}
+          icon={<Work />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Full Stack Engineer - Twitch
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            San Francisco, CA
+          </h4>
+          <p>
+            Helped the team launch 2 major features by working both in the front
+            end and back end.
+          </p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+      </div>
   )
 }
